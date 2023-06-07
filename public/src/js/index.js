748cf6d644a26fd474a96adcfc8b8e02c152a2b3
mengxain0913 <mengxian0913@gmail.com>
2023-06-08 02:49:59 +0800

    upload

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -15,33 +15,52 @@ arrow.addEventListener('click', function(){
         behavior: 'smooth'
     });
 
-    progressStartValue = 0;
     circle();
 })
 
 
 // sec2
 
-const circularProgress = document.querySelector(".circular-progress");
-const progressValue = document.querySelector(".progress-value");
+const circularProgress = document.querySelectorAll(".circular-progress");
+const progressValue = document.querySelectorAll(".progress-value");
 //parseInt(progressValue.innerHTML); 
-let progressStartValue = 0, progressEndValue = 70;
+let progressStartValue = [], progressEndValue = [60, 85, 70];
 const speed = 10;
 let progress_color = "rgba(73, 175, 231, 0.777)";
 
 
+for(var i=0;i<progressEndValue.length;i++){
+    progressStartValue.push(0);
+}
+
+let MAX_Progress_Value = -1;
+for(var i = 0; i<progressEndValue.length; i++){
+    MAX_Progress_Value = Math.max(progressEndValue[i], MAX_Progress_Value);
+}
+
+circle();
+
+
 function circle() {
 
+    for(var i = 0; i < progressStartValue.length; i++){
+        progressStartValue[i] = 0;
+    }
+
     progress = setInterval(() => {
-        progressStartValue++;
-        
-        progressValue.textContent = `${progressStartValue}%`;
-        circularProgress.style.background = `conic-gradient(${progress_color} ${progressStartValue * 3.6}deg, #ededed 0deg)`;
-        if(progressStartValue == progressEndValue){
-            clearInterval(progress);
+        for(var i = 0; i < progressStartValue.length; i++){
+            if(progressStartValue[i] < progressEndValue[i]){
+                progressStartValue[i]++;
+            }
+            console.log(i, progressStartValue[i]);
+            
+            progressValue[i].textContent = `${progressStartValue[i]}%`;
+            circularProgress[i].style.background = `conic-gradient(${progress_color} ${progressStartValue[i] * 3.6}deg, #ededed 0deg)`;
+            
+            if(progressStartValue[i] >= progressEndValue[i] && progressEndValue[i] == MAX_Progress_Value){
+                clearInterval(progress);
+            }
         }
-    
-    
     }, speed); 
 
     return;
