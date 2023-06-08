8e6ef23cabdc166fe42d8b3bdb768f08a910de45
mengxain0913 <mengxian0913@gmail.com>
2023-06-08 14:28:47 +0800

    upload

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -24,13 +24,19 @@ arrow.addEventListener('click', function(){
 const circularProgress = document.querySelectorAll(".circular-progress");
 const progressValue = document.querySelectorAll(".progress-value");
 //parseInt(progressValue.innerHTML); 
-let progressStartValue = [], progressEndValue = [60, 85, 70];
+let progressStartValue = [], progressEndValue = [];
 const speed = 10;
 let progress_color = "rgba(73, 175, 231, 0.777)";
 
-
-for(var i=0;i<progressEndValue.length;i++){
+for(var i=0;i<progressValue.length;i++){
+    var value = parseInt(progressValue[i].innerHTML);
+    progressEndValue.push(value);
     progressStartValue.push(0);
+}
+
+console.log("Hello1");
+for(var ii = 0; ii < 5; ii++){
+    setTimeout(() => console.log("Hello", ii), 1000)
 }
 
 let MAX_Progress_Value = -1;
@@ -39,7 +45,6 @@ for(var i = 0; i<progressEndValue.length; i++){
 }
 
 circle();
-
 
 function circle() {
 
@@ -51,9 +56,7 @@ function circle() {
         for(var i = 0; i < progressStartValue.length; i++){
             if(progressStartValue[i] < progressEndValue[i]){
                 progressStartValue[i]++;
-            }
-            console.log(i, progressStartValue[i]);
-            
+            }            
             progressValue[i].textContent = `${progressStartValue[i]}%`;
             circularProgress[i].style.background = `conic-gradient(${progress_color} ${progressStartValue[i] * 3.6}deg, #ededed 0deg)`;
             
